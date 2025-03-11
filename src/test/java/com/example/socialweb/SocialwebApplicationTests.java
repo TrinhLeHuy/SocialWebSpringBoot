71a7f81937ad34322c734c3826fa37b2d529b761
Trịnh Lê Huy <huytrinh.22032004@gmail.com>
2025-03-11 22:40:44 +0700

    setup project

---
+++ src/test/java/com/example/socialweb/SocialwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
